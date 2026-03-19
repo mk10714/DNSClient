1261f6b5fa1ab6b2e9d1678405389b8ec1cd0b4f
mk10714 <mk10714@nyu.edu>
2026-03-19 01:42:42 -0400

    DNSClient.py

--- DNSClient.py
+++ DNSClient.py
@@ -1,12 +1,8 @@
-
-
-
-
 import dns.resolver
 
 # Set the IP address of the local DNS server and a public DNS server
 local_host_ip = "127.0.0.1"
-real_name_server = "8.8.8.8" # Research public DNS servers to find a valid DNS server IP address to use
+real_name_server = "8.8.8.8"  # Research public DNS servers to find a valid DNS server IP address to use
 
 
 # Create a list of domain names to query - use the same list from the DNS Server
@@ -66,7 +62,7 @@ if __name__ == '__main__':
 
 
     # Call the function to print the results from querying both DNS servers
-    local_external_DNS_output(question_type)
+    #local_external_DNS_output(question_type)
     
     # Call the function to compare the results from both DNS servers and print the result
     result = compare_dns_servers(domainList,question_type)
